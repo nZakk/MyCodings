2de31ec5e484c26d360ed83f2c7b95c1a1e7484e
= <goncalvessantosisaac@gmail.com>
2024-05-05 14:28:40 -0400

    test

--- Java/java_course16_exercises/src/application/Program.java
+++ Java/java_course16_exercises/src/application/Program.java
@@ -1,7 +1,57 @@
-package application;
+package Java.java_course16_exercises.src.application;
+import Java.java_course16_exercises.src.entities.Client;
+
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter account number: ");
+        int accNumber = sc.nextInt();
+
+        sc.nextLine();
+        System.out.print("Enter account holder: ");
+        String name = sc.nextLine();
+
+        System.out.print("Is there an initial deposit (y/n)? ");
+        String answer = sc.nextLine();
+        
+        Client client1 = new Client(accNumber, name);
+        if (answer.equalsIgnoreCase("y")){
+            System.out.print("Enter initial deposit value:");
+            double initBalance = sc.nextDouble();
+            client1.deposit(initBalance);
+        }
+        
+        System.out.println();
+        
+        System.out.println("Account data:");
+        client1.ToString();
+        
+        System.out.println();
+
+        System.out.print("Enter a deposit value: ");
+        double amountMoney = sc.nextDouble();
+        client1.deposit(amountMoney);
+
+        System.out.println();
+
+        System.out.println("Updated account data:");
+        client1.ToString();
+
+        System.out.println();
+
+        System.out.print("Enter a withdraw value: ");
+        amountMoney = sc.nextDouble();
+        client1.withdraw(amountMoney);
+
+        System.out.println();
+
+        System.out.println("Updated account data:");
+        client1.ToString();
+
+
+        sc.close();
     }
 }
