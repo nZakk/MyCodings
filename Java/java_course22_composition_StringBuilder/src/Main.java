a461f2b3e5d4baa6e659d2685f3986bf88d3f509
= <goncalvessantosisaac@gmail.com>
2024-06-09 12:39:19 -0400

    aula22 composition w/ String builder

--- Java/java_course22_composition_StringBuilder/src/Main.java
+++ Java/java_course22_composition_StringBuilder/src/Main.java
@@ -1,5 +1,26 @@
+import enitities.Comment;
+import enitities.Post;
+
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Comment c1 = new Comment("Have a nice trip!");
+        Comment c2 = new Comment("Wow that's awesome");
+
+        Post p1 = new Post(LocalDateTime.parse("2018-06-21T13:05:44"), "Traveling to New Zealand", "I'm going to visit this wonderful country", 12);
+        p1.addComment(c1);
+        p1.addComment(c2);
+
+        Comment c3 = new Comment("Good Night");
+        Comment c4 = new Comment("may the force be with you!");
+
+        Post p2 = new Post(LocalDateTime.parse("2018-07-27T23:55:44"), "Good night guys!", "See you guys tomorrow", 8);
+        p2.addComment(c3);
+        p2.addComment(c4);
+
+        System.out.println(p1);
+        System.out.println(p2);
     }
 }
---
+++ Java/java_course22_composition_StringBuilder/src/enitities/Comment.java
@@ -0,0 +1,19 @@
+package enitities;
+
+public class Comment {
+    private String text;
+
+    public Comment(String text) {
+        this.text = text;
+    }
+
+    public Comment(){}
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
