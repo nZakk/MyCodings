7d806901c58540a7f40e36bc0833670b9c3a06e3
Isaac Santos <goncalvessantosisaac@gmail.com>
2024-06-10 18:05:06 -0400

    composition exercise

---
+++ Java/java_course23-composition_exercise1/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\033[1mThis is a bold too \033[0mIm not");
+
+        sc.close();
+    }
+}
