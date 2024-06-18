d5c62ffd451f078075ea16c0c7fc57516fcc76c1
Isaac Santos <goncalvessantosisaac@gmail.com>
2024-06-18 16:17:49 -0400

    aula34 veryBadWay exercise start

---
+++ Java/java_course34_verybadWay/src/model/entities/Reservation.java
@@ -0,0 +1,55 @@
+package model.entities;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Reservation {
+    private Integer roomNumber;
+    private LocalDate checkIn;
+    private LocalDate checkOut;
+
+    public Reservation(){}
+
+    public Reservation(Integer roomNumber, LocalDate checkIn, LocalDate checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public LocalDate getCheckIn() {
+        return checkIn;
+    }
+
+    public LocalDate getCheckOut() {
+        return checkOut;
+    }
+
+    public long duration(){
+        return Duration.between(checkIn, checkOut).toDays();
+    }
+
+    public void updateDates(LocalDate checkIn, LocalDate checkOut){
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString(){
+        return "Room " + roomNumber
+                + ", check-in: "
+                + DateTimeFormatter.ofPattern("dd/MM/yyyy").format(checkIn)
+                +  ", check-out: "
+                + DateTimeFormatter.ofPattern("dd/MM/yyyy").format(checkOut)
+                + ", " + duration()
+                + " nights";
+    }
+}
