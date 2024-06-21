528d9b53d78419a8ed2b8496773db56f60a571df
Isaac Santos <goncalvessantosisaac@gmail.com>
2024-06-21 15:47:07 -0400

    aula 37 update try with resources

--- Java/java-course37-FileReader-Bufferreader/src/Main.java
+++ Java/java-course37-FileReader-Bufferreader/src/Main.java
@@ -7,13 +7,8 @@ public class Main {
     public static void main(String[] args) {
 
         String path = "/home/isaac/Documents/conversa-goiania";
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
-
+        try(BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine();
 
             while (line != null){
@@ -24,18 +19,6 @@ public class Main {
         catch (IOException e){
             System.out.println(e.getMessage());
         }
-        finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-                }
-            }
-            catch (IOException e){
-                e.printStackTrace();
-            }
-        }
+
     }
 }
