e9d68dd6729c6fdab4be68f6d82526f310b95f8c
Isaac Santos <goncalvessantosisaac@gmail.com>
2024-06-27 14:59:06 -0400

    aula41

---
+++ Java/java_course41-File-exercise/src/Product.java
@@ -0,0 +1,42 @@
+public class Product {
+    private String name;
+    private Double price;
+    private Integer stock;
+
+    public Product(){
+    }
+
+    public Product(String name, Double price, Integer stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public Double totalStockValue(){
+        return price * stock;
+    }
+}
